73853d363644c7041d72171541a297af687c0bf1
Evan O'Connor <evanoc3@gmail.com>
2021-02-02 22:11:01 +0000

    Added user service to project

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import { withRouter, NextRouter } from "next/router";
 import styles from "./home.module.scss";
-import { AuthRequired, PageHead, HomeHeader } from "components/index";
-import authManager from "utils/auth-manager";
+import { AuthRequired, Background, Box, PageHead, HomeHeader } from "components/index";
 
 
 interface Props {
-	router: NextRouter
 }
 
 interface State {
@@ -19,20 +16,18 @@ class HomePage extends Component<Props, State> {
 			<AuthRequired>
 				<PageHead title="Home | Conversant" />
 
-				<div id={styles["page"]}>
+				<Background>
 					<HomeHeader />
-				</div>
+					<Box></Box>
+				</Background>
+
+				{/* <div id={styles["page"]}>
+
+				</div> */}
 			</AuthRequired>
 		);
 
 	}
-
-	public componentDidMount(): void {
-		// Redirect back to the login page if the user tries to access this page without being logged in
-		if(! authManager.isLoggedIn()) {
-			this.props.router.push("/login");
-		}
-	}
 }
 
-export default withRouter(HomePage);
+export default HomePage;
