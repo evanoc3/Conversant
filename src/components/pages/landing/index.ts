27f4fb28f8bbf35c9748fd4e75027a8a88bb465c
Evan O'Connor <evanoc3@gmail.com>
2021-02-10 01:34:24 +0000

    Removed old import from index file

--- src/components/pages/landing/index.ts
+++ src/components/pages/landing/index.ts
@@ -1,11 +1,7 @@
 import Header from "./Header";
-import HeroBox from "./HeroBox";
 import SearchInput from "./SearchInput";
-import SearchResults from "./SearchResults";
 
 export {
 	Header,
-	HeroBox,
 	SearchInput,
-	SearchResults
 };
