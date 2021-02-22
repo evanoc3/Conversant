db5ca3e6d37d9a7ff8b9208641d1d3607db601bf
Evan O'Connor <evanoc3@gmail.com>
2021-02-22 12:34:51 +0000

    CSS change for PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ const ConversantApp: FunctionComponent<AppProps> = ({ Component, pageProps }: Ap
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="application-name" content="Conversant" />
       <meta name="description" content="The natural language learning platform" />
-      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       <meta name="apple-mobile-web-app-title" content="Conversant" />
