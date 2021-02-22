13e511cc58de1628b0c28dcb0527e1584ca19e24
Evan O'Connor <evanoc3@gmail.com>
2021-02-22 00:10:36 +0000

    General trimming of fat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,6 @@ const ConversantApp: FunctionComponent<AppProps> = ({ Component, pageProps }: Ap
 
       {/* PWA meta tags */}
       <link rel="manifest" href="/static/manifest.json" />
-
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="application-name" content="Conversant" />
       <meta name="description" content="The natural language learning platform" />
@@ -22,10 +21,10 @@ const ConversantApp: FunctionComponent<AppProps> = ({ Component, pageProps }: Ap
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Conversant" />
       <meta name="format-detection" content="telephone=no" />
-      {/* <meta name="msapplication-config" content="/static/icons/browserconfig.xml" />
-      <meta name="msapplication-TileColor" content="#2B5797" />
-      <meta name="msapplication-tap-highlight" content="no" /> */}
-      <meta name="theme-color" content="#000000" />
+      {/* <meta name="msapplication-config" content="/static/icons/browserconfig.xml" /> */}
+      <meta name="msapplication-TileColor" content="#08060b" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#08060b" />
       <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
       <link rel="shortcut icon" href="/icons/favicon.ico" />
       <link rel="apple-touch-icon" sizes="192x192" href="/icons/logo-192.png" />
