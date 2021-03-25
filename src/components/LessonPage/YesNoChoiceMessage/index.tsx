7d7b65a595ffeec49944056fed06331b6f80a74f
Evan O'Connor <evanoc3@gmail.com>
2021-03-25 14:27:55 +0000

    Updated YesNoChoiceMessage

--- src/components/LessonPage/YesNoChoiceMessage/index.tsx
+++ src/components/LessonPage/YesNoChoiceMessage/index.tsx
@@ -1,4 +1,5 @@
 import type { FunctionComponent, PropsWithChildren } from "react";
+import { useState, useEffect } from "react";
 import styles from "./YesNoChoiceMessage.module.scss";
 
 
@@ -11,10 +12,26 @@ type Props = PropsWithChildren<{
 
 const YesNoChoiceMessage: FunctionComponent<Props> = (props) => {
 
+	const [yesButtonDisabled, setYesButtonDisabled] = useState(false);
+	const [noButtonDisabled, setNoButtonDisabled] = useState(false);
+
+
 	function choiceSelectionHandlerFactory(choice: boolean) {
-		const choiceSelectionHandler = (props.onChoiceSelected == undefined) ? () => {} : props.onChoiceSelected;
-		return () => choiceSelectionHandler(choice);
+		const choiceSelectionHandler = (props.onChoiceSelected === undefined) ? () => {} : props.onChoiceSelected;
+		return () => {
+			setYesButtonDisabled(choice === false);
+			setNoButtonDisabled(choice === true);
+			choiceSelectionHandler(choice);
+		}
 	}
+
+
+	useEffect(() => {
+		setYesButtonDisabled( props.choiceSelected !== undefined && props.choiceSelected !== true );
+		setNoButtonDisabled( props.choiceSelected !== undefined && props.choiceSelected !== false );
+	}, [ props.choiceSelected ]);
+
+
 
 	return (
 		<div className={styles["container"]}>
@@ -23,11 +40,11 @@ const YesNoChoiceMessage: FunctionComponent<Props> = (props) => {
 					<span>{ props.message }</span>
 				</div>
 
-				<button className={styles["message-choice"]} onClick={choiceSelectionHandlerFactory(true)} disabled={props.choiceSelected === false}>
+				<button className={styles["message-choice"]} onClick={choiceSelectionHandlerFactory(true)} disabled={yesButtonDisabled}>
 					Yes
 				</button>
 
-				<button className={styles["message-choice"]} onClick={choiceSelectionHandlerFactory(false)} disabled={props.choiceSelected === true}>
+				<button className={styles["message-choice"]} onClick={choiceSelectionHandlerFactory(false)} disabled={noButtonDisabled}>
 					No
 				</button>
 			</div>
