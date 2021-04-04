3b171d27260c9e3b402915fa059328321fd8619a
Evan O'Connor <evanoc3@gmail.com>
2021-04-04 14:38:14 +0100

    Styled LessonCarousel some more

--- src/components/TopicPage/LessonCarousel/index.tsx
+++ src/components/TopicPage/LessonCarousel/index.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import Link from "next/link";
-import { ArrowRight as ArrowRightSvg } from "react-feather";
+import { ArrowRight as ArrowRightSvg, ExternalLink as ExternalLinkSvg } from "react-feather";
 import styles from "./LessonCarousel.module.scss";
 
 import { FunctionComponent, PropsWithChildren } from "react";
@@ -16,34 +16,47 @@ const LessonCarousel: FunctionComponent<Props> = (props) => {
 	return (
 		<div id={styles["container"]}>
 			<div className={styles["h-spacer"]} />
-				{
-					(props.lessons.length >= 1) ? (
-						props.lessons.map((lesson, index) => {
-							return (
-								<Fragment key={lesson.id}>
-									<ArrowRightSvg className={styles["arrow"]} />
-	
-									<div className={styles["lesson-container"]}>
-										<h2>{index + 1}. { lesson.title }</h2>
-	
+
+			{
+				(props.lessons.length >= 1) ? (
+					props.lessons.map((lesson, index) => {
+						return (
+							<Fragment key={lesson.id}>
+								<ArrowRightSvg className={styles["arrow"]} />
+
+								<div className={styles["lesson-container"]}>
+									<h2>{index + 1}. { lesson.title }</h2>
+									<div className={styles["lesson-description-container"]}>
 										{
 											(lesson.description !== null) ? (
 												<p>{ lesson.description }</p>
 											) : ""
 										}
-										
-										<Link href={lesson.href}>
-											<a href={lesson.href}>Go to lesson</a>
-										</Link>
 									</div>
-								</Fragment>
-							)
-						})
-					) : (
-						<div>No Lessons Available</div>
-					)
-				}
-			{/* </div> */}
+									
+									<div className={styles["lesson-bottom-row"]}>
+										<Link href={lesson.href}>
+											<a href={lesson.href}>
+												Go to lesson
+												<ExternalLinkSvg className={styles["lesson-link-icon"]} />
+											</a>
+										</Link>
+
+										<div className={styles["lesson-h-spacer"]} />
+
+										{
+
+										}
+									</div>
+									
+								</div>
+							</Fragment>
+						)
+					})
+				) : (
+					<div>No Lessons Available</div>
+				)
+			}
 
 			<div className={styles["h-spacer"]} />
 		</div>
