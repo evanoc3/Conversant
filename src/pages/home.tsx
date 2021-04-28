bf61277320a626940ce6783ee0caa446403aee09
Evan O'Connor <evanoc3@gmail.com>
2021-04-28 14:25:22 +0100

    Added a label with the user's last sign in timestamp to the home page.
    Also refactored this page's component a bit, as part of work to add more functionality to it

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,31 +1,36 @@
-import type { PropsWithChildren, MouseEventHandler } from "react";
-import type { NextPage } from "next";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
-import { useSession, signOut } from "next-auth/client";
+import { useSession } from "next-auth/client";
 import { Background } from "@components/index";
 import { Header, LessonList } from "@components/HomePage/index";
 import styles from "./home.module.scss";
+
+import type { PropsWithChildren } from "react";
+import type { Response as LastSigninApiRouteResponse } from "@pages/api/my/last-signin";
 
 
 type Props = PropsWithChildren<{
 }>
 
 
-const HomePage: NextPage<Props> = (props) => {
-	const [session, loading] = useSession();
+export default function HomePage(props: Props): JSX.Element {
+	const router = useRouter();
+	const [session, sessionLoading] = useSession();
+	const [lastSignInTime, setLastSignInTime] = useState<Date | null>(null);
 
-	if(!loading && !session) {
-		const router = useRouter();
-		router.replace("/");
-	}
+	useEffect(() => {
+		if(router.isReady && !sessionLoading && session === null) {
+			router.replace("/");
+		}
+	}, [ router.isReady, sessionLoading, session ]);
 
-	const signOutClickHandler: MouseEventHandler<HTMLAnchorElement> = (e) => {
-		e.preventDefault();
-		signOut({
-			callbackUrl: "/"
-		});
-	};
+	useEffect(() => {
+		if(session !== null) {
+			getLastSignInTime().then(time => setLastSignInTime(time)).catch(err => { throw err; });
+		}
+	}, [ session ]);
+
 
 	return (
 		<>
@@ -37,17 +42,30 @@ const HomePage: NextPage<Props> = (props) => {
 				<Header />
 
 				<div id={styles["main"]}>
-					{
-						(!loading && session) ? (
-							<LessonList />
-						) : (
-							<span>Loading...</span>
-						)
-					}
+					Last sign in was at: { lastSignInTime?.toISOString() ?? "---" }
+					<LessonList />
 				</div>
 			</Background>
 		</>
 	);
 }
 
-export default HomePage;
+
+/**
+ * Helper function which queries the API for the datetime user's latest session.
+ */
+async function getLastSignInTime(): Promise<Date> {
+	const resp = await fetch("/api/my/last-signin");
+
+	if(!resp.ok) {
+		throw new Error(`Failed to retrieve last signin time. Received status \"${resp.statusText}\" from API`);
+	}
+
+	const body = await resp.json() as LastSigninApiRouteResponse;
+
+	if("error" in body) {
+		throw new Error(body.error);
+	}
+
+	return new Date(body.lastSignInTime);
+}
